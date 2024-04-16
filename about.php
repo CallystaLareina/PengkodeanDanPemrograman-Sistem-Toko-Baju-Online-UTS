5585ce1f5dcdf27dfb66fd617e4bc00d5ef1cede
CallystaLareina <167252595+CallystaLareina@users.noreply.github.com>
2024-04-17 04:14:25 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #de73ff"><b>Tentang Kami</b></h2>
+	<p class="text-justify">Selamat datang di Toko Baju Callysta, destinasi fashion Anda untuk gaya sehari-hari yang chic dan trendi. Kami memahami betapa pentingnya penampilan dalam mengekspresikan diri, itulah mengapa kami berdedikasi untuk menyediakan koleksi baju yang tidak hanya modis tetapi juga nyaman untuk dikenakan sepanjang hari.</p>
+<p>
+Pelayanan pelanggan adalah prioritas utama kami. Tim kami selalu siap membantu Anda menemukan pilihan baju yang tepat sesuai dengan selera dan kebutuhan Anda. Nikmati pengalaman berbelanja yang mudah, cepat, dan aman dengan pilihan pengiriman yang fleksibel ke seluruh Indonesia.</p>
+</div>
+
+
+
+
+ <?php 
+	include 'footer.php';
+ ?>
